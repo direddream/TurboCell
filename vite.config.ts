8b493c2aaa56269ecc2393648f353cccbfb94b23
direddream <851413717@qq.com>
2026-01-04 22:17:21 +0800

    打包项目并推送静态文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TurboCell/' // 关键：适配GitHub Pages的仓库路径
 })
